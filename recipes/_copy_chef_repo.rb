a5161f2a17450b1a94ae9ed415794ca40a2adbf1
Hippie Hacker <chris@hippiehacker.org>
2012-12-27 12:55:18 -0800

    copying chef-repo onto usb for later use

---
+++ recipes/_copy_chef_repo.rb
@@ -0,0 +1,10 @@
+# Make sure it's unmounted before we possibly format it
+
+target_repo_dir = "#{node['ii-usb']['target-mountpoint']}/chef-repo"
+directory target_repo_dir
+
+# This is not idempotent, and is an ugly hack... fixme later
+execute 'rsync chef-repo onto usb' do
+  command "rsync -rlptDv #{node['ii-usb']['src-chef-repo']}/ #{target_repo_dir}/"
+  not_if 
+end
