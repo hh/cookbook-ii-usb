e1b084de1fb3d796dc7fd523a05c4d74e7320bb1
Hippie Hacker <chris@hippiehacker.org>
2013-01-02 16:09:45 -0800

    refactor command and solo.rb file resources

--- recipes/_configure_grub.rb
+++ recipes/_configure_grub.rb
@@ -11,25 +11,23 @@ template "#{usb}/boot/grub/x86_64-efi/grub.cfg" do
   source 'efi-grub.cfg.erb'
 end
 
+# The seedfile should be templated out more
 template "#{usb}/preseed.seed" do
   source 'preseed.seed.erb'
 end
 
-# The three places we can run custom commands
-
-template "#{usb}/initramfs_command.sh" do
-  source 'initramfs_command.sh.erb'
+# The four places we can run custom commands
+['initramfs_command','early_command','success_command','firstboot'].each do |script|
+  template "#{usb}/#{script}.sh" do
+    source "#{script}.sh.erb"
+  end
 end
 
-template "#{usb}/early_command.sh" do
-  source 'early_command.sh.erb'
+['initramfs','early','success'].each do |stage|
+  template "#{usb}/ubiquity-#{stage}-solo.rb" do
+    source "ubiquity-solo.rb.erb"
+    variables({
+        stage: stage
+      })
+  end
 end
-
-template "#{usb}/success_command.sh" do
-  source 'success_command.sh.erb'
-end
-
-template "#{usb}/firstboot.sh" do
-  source 'firstboot.sh.erb'
-end
-
